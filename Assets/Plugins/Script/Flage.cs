2d0b26d690d9229cf7715fe70b3379716ece3f28
OleWang47 <bohaowang47@gmail.com>
2024-08-24 20:14:47 +0800

    整合版alpha

---
+++ Assets/Plugins/Script/Flage.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flage : MonoBehaviour
+{
+    public void Touch()
+    {
+        print("touch");
+    }
+}
