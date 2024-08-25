8ca7d31ce3bb2d48eb7925bdd7fa2c891ce32ff0
OleWang47 <bohaowang47@gmail.com>
2024-08-25 12:45:57 +0800

    試玩一版

---
+++ Assets/Scripts/FinshLine.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinshLine : MonoBehaviour
+{
+    private LayerMask playerLayer;
+    private void Start()
+    {
+        playerLayer = LayerMask.NameToLayer("Player");
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer == playerLayer)
+        {
+            
+        }
+    }
+}
